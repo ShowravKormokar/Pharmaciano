405566476b07dfc8783f330cabee46ab8b08e0d8
ShowravKormokar <showravkormokar354@gmail.com>
2025-11-01 01:57:06 +0600

    Remove unuse spinner varients

--- frontend/src/components/ui/shadcn-io/spinner/index.tsx
+++ frontend/src/components/ui/shadcn-io/spinner/index.tsx
@@ -12,35 +12,6 @@ const Default = ({ className, ...props }: SpinnerVariantProps) => (
   <LoaderIcon className={cn('animate-spin', className)} {...props} />
 );
 
-const Circle = ({ className, ...props }: SpinnerVariantProps) => (
-  <LoaderCircleIcon className={cn('animate-spin', className)} {...props} />
-);
-
-const Pinwheel = ({ className, ...props }: SpinnerVariantProps) => (
-  <LoaderPinwheelIcon className={cn('animate-spin', className)} {...props} />
-);
-
-const CircleFilled = ({
-  className,
-  size = 24,
-  ...props
-}: SpinnerVariantProps) => (
-  <div className="relative" style={{ width: size, height: size }}>
-    <div className="absolute inset-0 rotate-180">
-      <LoaderCircleIcon
-        className={cn('animate-spin', className, 'text-foreground opacity-20')}
-        size={size}
-        {...props}
-      />
-    </div>
-    <LoaderCircleIcon
-      className={cn('relative animate-spin', className)}
-      size={size}
-      {...props}
-    />
-  </div>
-);
-
 const Ellipsis = ({ size = 24, ...props }: SpinnerVariantProps) => {
   return (
     <svg
