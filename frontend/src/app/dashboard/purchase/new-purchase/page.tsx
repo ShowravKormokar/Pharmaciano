5815b7c59dd454117fa26d82e98e5b63f247a9f5
ShowravKormokar <showravkormokar354@gmail.com>
2026-02-03 20:08:06 +0600

    Make purchase all features structure

---
+++ frontend/src/app/dashboard/purchase/new-purchase/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewPurchase = () => {
+    return (
+        <div>NewPurchase</div>
+    )
+}
+
+export default NewPurchase;
---
+++ frontend/src/app/dashboard/purchase/purchase-return-list/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PurchaseReturnList = () => {
+    return (
+        <div>PurchaseReturnList</div>
+    )
+}
+
+export default PurchaseReturnList;
