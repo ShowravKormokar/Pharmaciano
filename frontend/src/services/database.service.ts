fa8eab147ee504540476c10ede04e0522c03ac89
ShowravKormokar <showravkormokar354@gmail.com>
2026-05-29 21:40:23 +0600

    [Fix]:type error

--- frontend/src/services/database.service.ts
+++ frontend/src/services/database.service.ts
@@ -1,7 +1,7 @@
 import { api } from "@/lib/api";
 
 export const downloadDatabaseBackupService = async (): Promise<Blob> => {
-    const res = await api.get("/v1/database/download", {
+    const res = await api.get<Blob>("/v1/database/download", {
         responseType: "blob",
         headers: { Accept: "application/json" },
     });
